2c381967ac9e107850f7d473dfb530ea2a19ba02
kavya.pl <kavya.pl@gmail.com>
2018-12-27 08:40:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'perfect';
+  showDiv = false;
+  mySection = "";
+  title = "perfect";
+  name = "Angular";
+  tools = ["Angular", "React", "Node"];
+
+  giveName() {
+    // alert("Good morning");
+    this.showDiv = !this.showDiv;
+
+    // false  =
+  }
+
+  changeSection(section, x) {
+    this.mySection = section;
+  }
 }
